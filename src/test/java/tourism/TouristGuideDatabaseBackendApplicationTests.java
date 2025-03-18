017f4755190a3af832432af8a955c904e6215602
huw02 <hannibal@ussing.com>
2025-03-18 16:27:33 +0100

    ..

--- src/test/java/tourism/TouristGuideDatabaseBackendApplicationTests.java
+++ src/test/java/tourism/TouristGuideDatabaseBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.touristguidedatabasebackend;
+package tourism;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/tourism/controller/TouristControllerTest.java
+++ src/test/java/tourism/controller/TouristControllerTest.java
@@ -1,4 +1,4 @@
-package org.example.touristguidedatabasebackend.controller;
+package tourism.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
